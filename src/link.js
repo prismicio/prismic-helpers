8265fc0b9c1679591f80a16a6efc908e057a9743
Hugo Villain <hugo.villain@prismic.io>
2019-11-04 16:51:25 +0100

    Refactor Link for GraphQL API

--- src/link.js
+++ src/link.js
@@ -1,9 +1,19 @@
 export default {
   url(link, linkResolver) {
-    if(link.link_type === 'Document') {
-      return linkResolver ? linkResolver(link, link.isBroken) : '';
-    } else {
+    if (
+      link &&
+      [link.link_type, link._linkType, link.linkType].some(
+        e => e && ["Document", "Link.Document", "Link.document"].includes(e)
+      )
+    ) {
+      return linkResolver ? linkResolver(link) : "";
+    }
+    if (link) {
       return link.url;
     }
+    if (process.env.NODE_ENV !== "production") {
+      console.warn('PrismicHelpers/Link.url expects a Prismic "link" object as first argument but none was passed');
+    }
+    return "";
   }
-}
+};
