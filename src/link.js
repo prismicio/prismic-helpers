7cc4cc0d2b1bbbbe149d8fb482fc59509ddbb203
Hugo Villain <hugo.villain@prismic.io>
2020-04-14 16:10:48 +0200

    post conversation: prioritize link resolver for complex cases

--- src/link.js
+++ src/link.js
@@ -1,15 +1,15 @@
 export default {
   url(link, linkResolver) {
-    if (link && link.url) {
-      return link.url;
-    }
     if (
       link &&
       [link.link_type, link._linkType, link.linkType].some(
         e => e && ["Document", "Link.Document", "Link.document"].includes(e)
       )
     ) {
-      return linkResolver ? linkResolver(link) : "";
+      return linkResolver ? linkResolver(link) : link.url;
+    }
+    if (link && link.url) {
+      return link.url;
     }
     if (process.env.NODE_ENV !== "production") {
       console.warn('PrismicHelpers/Link.url expects a Prismic "link" object as first argument but none was passed');
