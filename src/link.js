cfb57521b6c3f46361f54faf9486aae7e1785c9f
hypervillain <hugo.villain@prismic.io>
2020-08-07 15:56:22 +0200

    Fix new link format

--- src/link.js
+++ src/link.js
@@ -1,18 +1,19 @@
 module.exports = {
   url(link, linkResolver) {
+    const doc = link && link.value ? link.value.document : link
     if (
       link &&
-      [link.link_type, link._linkType, link.linkType].some(
+      [link.type, link.link_type, link._linkType, link.linkType].some(
         e => e && ["Document", "Link.Document", "Link.document"].includes(e)
       ) && linkResolver && typeof linkResolver === 'function'
     ) {
-      const url = linkResolver(link)
+      const url = linkResolver(doc)
       if (url) {
         return url
       }
     }
-    if (link && link.url) {
-      return link.url;
+    if (doc && doc.url) {
+      return doc.url;
     }
     if (process.env.NODE_ENV !== "production") {
       console.warn('PrismicHelpers/Link.url expects a Prismic "link" object as first argument but none was passed');
