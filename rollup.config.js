f182777c4714c5405ffb077de2ad1934d9554be2
hypervillain <hugo.villain@prismic.io>
2020-09-14 16:10:31 +0200

    Add rollup-plugin-size

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { babel } from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
+import size from 'rollup-plugin-size'
 
 const input = './src/index.js';
 const name = "PrismicHelpers";
@@ -9,36 +10,36 @@ export default [
   {
     input,
     output: {
-      file: './dist/prismic-helpers.esm.js',
-      format: 'esm'
+      file: "./dist/prismic-helpers.esm.js",
+      format: "esm",
     },
-    plugins: [
-      babel({ babelHelpers: 'bundled' })
-    ]
+    plugins: [babel({ babelHelpers: "bundled" }), size()],
   },
   {
     input,
     output: {
-      file: './dist/prismic-helpers.js',
-      format: 'umd',
-      name
+      file: "./dist/prismic-helpers.js",
+      format: "umd",
+      name,
     },
     plugins: [
-      babel({ babelHelpers: 'bundled' }),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('development') })
-    ]
+      babel({ babelHelpers: "bundled" }),
+      replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
+      size(),
+    ],
   },
   {
     input,
     output: {
-      file: './dist/prismic-helpers.min.js',
-      format: 'umd',
-      name
+      file: "./dist/prismic-helpers.min.js",
+      format: "umd",
+      name,
     },
     plugins: [
-      babel({ babelHelpers: 'bundled' }),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      terser()
-    ]
-  }
+      babel({ babelHelpers: "bundled" }),
+      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      terser(),
+      size(),
+    ],
+  },
 ];
