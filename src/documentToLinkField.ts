ef16ef58ed730df6690c08fc37a4b65f4e5027e1
lihbr <lihbr@users.noreply.github.com>
2021-05-11 17:42:59 +0200

    refactor: extract document cast to link field into its own function

---
+++ src/documentToLinkField.ts
@@ -0,0 +1,28 @@
+import {
+	FilledLinkToDocumentField,
+	LinkType,
+	PrismicDocument
+} from "@prismicio/types";
+
+/**
+ * Converts a document into a link field
+ * This is useful when crawling the API for document links
+ *
+ * @param prismicDocument - A document coming from Prismic
+ *
+ * @returns The equivalent link field to use with `asLink()`
+ */
+export function documentToLinkField(
+	prismicDocument: PrismicDocument
+): FilledLinkToDocumentField {
+	return {
+		link_type: LinkType.Document,
+		id: prismicDocument.id,
+		uid: prismicDocument.uid ?? undefined,
+		type: prismicDocument.type,
+		tags: prismicDocument.tags,
+		lang: prismicDocument.lang,
+		url: prismicDocument.url ?? undefined,
+		slug: prismicDocument.slugs[0] // Slug field not available with GraphQl
+	};
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { asDate } from "./asDate";
 export { asLink } from "./asLink";
 
+export { documentToLinkField } from "./documentToLinkField";
+
 export type { LinkResolverFunction } from "./types";
