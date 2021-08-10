715f11db3776a189fcc9f59e835f08900e3059be
lihbr <lihbr@users.noreply.github.com>
2021-08-10 18:30:53 +0200

    feat: export serializer elements types for convenience

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ export { asHTML } from "./asHTML";
 export { documentToLinkField } from "./documentToLinkField";
 export { documentAsLink } from "./documentAsLink";
 
+export { Element } from "@prismicio/richtext";
+
 export type {
 	LinkResolverFunction,
 	HTMLFunctionSerializer,
