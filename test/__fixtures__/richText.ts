06be5373133011a5a4e70eac10a4ca301f8372b8
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-09-14 11:48:46 -1000

    test: add explicit test for links with a target

--- test/__fixtures__/richText.ts
+++ test/__fixtures__/richText.ts
@@ -2,10 +2,8 @@ import { RichTextField } from "@prismicio/types";
 
 import enRichTextJSON from "./enRichText.json";
 import xssRichTextJSON from "./xssRichText.json";
-import noLinkTargetRichTextJSON from "./noLinkTargetRichText.json";
 
 export const richTextFixture = {
 	en: enRichTextJSON as RichTextField,
 	xss: xssRichTextJSON as RichTextField,
-	noLinkTarget: noLinkTargetRichTextJSON as RichTextField,
 };
--- test/asHTML.test.ts
+++ test/asHTML.test.ts
@@ -1,4 +1,5 @@
 import { it, expect } from "vitest";
+import { RichTextField } from "@prismicio/types";
 
 import { richTextFixture } from "./__fixtures__/richText";
 import { linkResolver } from "./__testutils__/linkResolver";
@@ -27,8 +28,53 @@ it("escapes external links to prevent XSS", () => {
 	expect(asHTML(richTextFixture.xss, linkResolver)).toMatchSnapshot();
 });
 
-it("omits the target attributes on links without a target value", () => {
-	expect(asHTML(richTextFixture.noLinkTarget, linkResolver)).toMatchSnapshot();
+it("omits target attribute on links without a target value", () => {
+	const field: RichTextField = [
+		{
+			type: "paragraph",
+			text: "link",
+			spans: [
+				{
+					type: "hyperlink",
+					start: 0,
+					end: 4,
+					data: {
+						link_type: "Web",
+						url: "https://example.org",
+					},
+				},
+			],
+		},
+	];
+
+	expect(asHTML(field, linkResolver)).toMatchInlineSnapshot(
+		'"<p><a href=\\"https://example.org\\" rel=\\"noopener noreferrer\\">link</a></p>"',
+	);
+});
+
+it("includes target attribute on links with a target value", () => {
+	const field: RichTextField = [
+		{
+			type: "paragraph",
+			text: "link",
+			spans: [
+				{
+					type: "hyperlink",
+					start: 0,
+					end: 4,
+					data: {
+						link_type: "Web",
+						url: "https://example.org",
+						target: "_blank",
+					},
+				},
+			],
+		},
+	];
+
+	expect(asHTML(field, linkResolver)).toMatchInlineSnapshot(
+		'"<p><a href=\\"https://example.org\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">link</a></p>"',
+	);
 });
 
 it("returns null for nullish inputs", () => {
